d7c7a775df245b59035a3f78fc4bd410168eac54
linnea <linneagodunov@protonmail.com>
2025-08-21 09:56:34 +0300

    teht2

---
+++ mod-2/teht3.py
@@ -0,0 +1,10 @@
+#Kirjoita ohjelma, joka kysyy suorakulmion kannan ja korkeuden.
+#Ohjelma tulostaa suorakulmion piirin ja pinta-alan.
+
+import math
+
+käyttäjä = input('Anna suorakulmion kanta: ')
+käyttäjä = input('Anna suorakulmion korkeus: ')
+
+
+
