29211b5407aa723af0513b549cd4bd041e854b73
linnea <linneagodunov@protonmail.com>
2025-08-24 19:24:00 +0300

    teht4

--- mod-2/teht4.py
+++ mod-2/teht4.py
@@ -1,3 +1,22 @@
 #  Kirjoita ohjelma, joka kysyy kolme kokonaislukua.
 #  Ohjelma tulostaa lukujen summan, tulon ja keskiarvon
 
+yy_str = input("Anna ensimmäinen kokonaisluku: ")
+yy = float(yy_str)
+
+kaa_str = input("Anna toinen kokonaisluku: ")
+kaa = float(kaa_str)
+
+koo_str = input("Anna kolmas kokonaisluku: ")
+koo = float(koo_str)
+
+summa = yy + kaa + koo
+tulo = yy * kaa * koo
+keskiarvo = summa /3
+
+print("Lukujen summa on " + str(summa))
+print("Lukujen tulo on " + str(tulo))
+print("Lukujen keskiarvo on " + str(keskiarvo))
+
+
+
