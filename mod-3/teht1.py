35bd888165b46a1f2cf7a4ed0557828937bc71a2
linnea <linneagodunov@protonmail.com>
2025-08-26 14:55:16 +0300

    teht 1

---
+++ mod-3/teht1.py
@@ -0,0 +1,15 @@
+
+# Kirjoita ohjelma, joka kysyy kalastajalta kuhan pituuden senttimetreinä.
+# Jos kuha on alamittainen, ohjelma käskee laskea kuhan takaisin järveen ilmoittaen samalla käyttäjälle,
+# montako senttiä alimmasta sallitusta pyyntimitasta puuttuu. Kuha on alamittainen, jos sen pituus on alle 37 cm.
+
+kala = input("Kuinka pitkä kalastamasi kuha on senttimetreissä? ")
+kala = float(kala)
+
+alipaino = 37 - kala
+alipaino = str(alipaino)
+
+if kala < 37:
+    print("Laske kuha taikaisin veteen. Kuha on " + alipaino + " senttiä alimittaien.")
+
+
