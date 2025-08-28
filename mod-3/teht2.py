9de6ef84f863a82fe14b3da1bca86a040a7ffa86
linnea <linneagodunov@protonmail.com>
2025-08-28 20:01:36 +0300

    teht2

--- mod-3/teht2.py
+++ mod-3/teht2.py
@@ -1,15 +1,17 @@
 
-# Kirjoita ohjelma, joka kysyy käyttäjältä laivan hyttiluokan (LUX, A, B, C)
-# ja tulostaa sen sanallisen kuvauksen alla olevan luettelon mukaisesti.
-# Tehtävässä on käytettävä if/elif/else-toistorakennetta.
-# LUX on parvekkeellinen hytti yläkannella.
-# A on ikkunallinen hytti autokannen yläpuolella.
-# B on ikkunaton hytti autokannen yläpuolella.
-# C on ikkunaton hytti autokannen alapuolella.
-# Jos käyttäjä syöttää kelvottoman hyttiluokan, ohjelma tulostaa Virheellinen hyttiluokka.
+hytti = input("Minkä hyttiluokan valitset; LUX, A, B, vai C? ")
 
-hytti = input("Minkä hyttiluokan valitset? Hyttiluokat ovat seuraavat: ")
-print("LUX on parvekkeellinen hytti yläkannella.")
-print("A on ikkunallinen hytti autokannen yläpuolella.")
-print("B on ikkunaton hytti autokannennen yläpuolella.")
-print("C on ikkunaton hytti autokannen alapuolella")
+if hytti == "LUX" or hytti == "Lux" or hytti == "lux":
+    print("LUX on parvekkeellinen hytti yläkannella.")
+
+elif hytti == "A" or hytti == "a":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+
+elif hytti == "B" or hytti == "b":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+
+elif hytti == "C" or hytti == "c":
+    print("C on ikkunaton hytti autokannen alapuolella.")
+
+else:
+    print("Virheellinen hyttiluokka. Valitse yksi listatuista luokista.")
