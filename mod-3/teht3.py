55a4e4df654f3b45dfe057a38386e103ee909d66
linnea <linneagodunov@protonmail.com>
2025-08-28 20:59:36 +0300

    teht3

--- mod-3/teht3.py
+++ mod-3/teht3.py
@@ -1,5 +1,30 @@
 
-#Kirjoita ohjelma, joka kysyy käyttäjän biologisen sukupuolen ja hemoglobiiniarvon (g/l).
-# Ohjelma ilmoittaa, onko hemoglobiiniarvo alhainen, normaali vai korkea.
-# Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.
-# Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.
+
+sukupuoli = input("Kerro biologinen sukupuolesi (mies/nainen): ")
+hemo = int(input("Kerro hemoglobiiniarvosi (g/l): "))
+
+hemo_normaali_m = 134 <= hemo <= 195
+hemo_normaali_n = 117 <= hemo <= 175
+
+hemo_korkea_m = hemo > 195
+hemo_korkea_n = hemo > 174
+
+hemo_alhainen_m = hemo < 135
+hemo_alhainen_n = hemo < 117
+
+if sukupuoli == "mies":
+    if hemo_normaali_m:
+        print("Hemoglobiinitasosi on normaali.")
+    elif hemo_korkea_m:
+        print("Hemoglobiinitasosi on korkea.")
+    else:
+        print("Hemoglobiinitasosi on alhainen.")
+
+if sukupuoli == "nainen":
+    if hemo_normaali_n:
+        print("Hemoglobiinitasosi on normaali.")
+    elif hemo_korkea_n:
+        print("Hemoglobiinitasosi on korkea.")
+    else:
+        print("Hemoglobiinitasosi on alhainen.")
+
