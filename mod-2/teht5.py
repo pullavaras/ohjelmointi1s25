8703cf7f0bb2ae8216564f16d12565bee617cdb2
pullavaras <linneagodunov@protonmail.com>
2025-08-30 22:44:54 +0300

    korjattu koodi

--- mod-2/teht5.py
+++ mod-2/teht5.py
@@ -1,28 +1,17 @@
-#Kirjoita ohjelma, joka kysyy käyttäjältä massan keskiaikaisten mittojen mukaan leivisköinä, nauloina ja luoteina.
-# Ohjelma muuntaa syötteen täysiksi kilogrammoiksi ja grammoiksi sekä ilmoittaa tuloksen käyttäjälle.
 
-#   Yksi leiviskä on 20 naulaa.
-#   Yksi naula on 32 luotia.
-#   Yksi luoti on 13,3 grammaa.
 
-leiviskat = input("Anna leiviskät.\n")
-leiviskat = float(leiviskat)
+leiviska = float(input("Anna leiviskät: "))
+naula = float(input("Anna naulat: "))
+luoti = float(input("Anna luodit: "))
 
-naulat = input("Anna naulat.\n")
-naulat = float(naulat)
 
-luodit = input("Anna luodit.\n")
-luodit = float(luodit)
+luoti_g = luoti * 13.3
+naula_g = naula * 32 * 13.3
+leiviska_g = naula * 20 * 32 * 13.3
 
-luoti = 13.3
-naula = luoti * 32
-leiviska = naula * 20
+gramma = luoti_g + naula_g + leiviska_g
+kilo = gramma / 1000
 
-paino_yht = leiviskat * leiviska + naulat * naula + luodit * luoti
-kilo = paino_yht / 1000
-gramma = kilo * 1000
-
-print("Massa nykymittojen mukaan:")
-print(f"{kilo:.2f} kilogrammaa ja {gramma:.2f} grammaa.")
+print(f"Massa nykymittojen mukaan:\n{kilo:.2f} kilogrammaa ja {gramma:.2f} grammaa.")
 
 
