0b2410f090279569dfe7c7f4e74a7a68421ca353
pullavaras <linneagodunov@protonmail.com>
2025-08-31 13:32:10 +0300

    teht3

--- mod-3/teht3.py
+++ mod-3/teht3.py
@@ -1,30 +1,18 @@
-
-
 sukupuoli = input("Kerro biologinen sukupuolesi (mies/nainen): ")
-hemo = int(input("Kerro hemoglobiiniarvosi (g/l): "))
-
-hemo_normaali_m = 134 <= hemo <= 195
-hemo_normaali_n = 117 <= hemo <= 175
-
-hemo_korkea_m = hemo > 195
-hemo_korkea_n = hemo > 174
-
-hemo_alhainen_m = hemo < 135
-hemo_alhainen_n = hemo < 117
+hemoglobiini = int(input("Anna hemoglobiiniarvosi (g/l): "))
 
 if sukupuoli == "mies":
-    if hemo_normaali_m:
+    if 134 <= hemoglobiini < 195:
         print("Hemoglobiinitasosi on normaali.")
-    elif hemo_korkea_m:
-        print("Hemoglobiinitasosi on korkea.")
-    else:
+    elif hemoglobiini > 195:
+        print("Hemoglobiinitasosi on korkea." )
+    elif hemoglobiini < 134:
         print("Hemoglobiinitasosi on alhainen.")
 
 if sukupuoli == "nainen":
-    if hemo_normaali_n:
+    if 117 <= hemoglobiini < 175:
         print("Hemoglobiinitasosi on normaali.")
-    elif hemo_korkea_n:
+    elif hemoglobiini > 175:
         print("Hemoglobiinitasosi on korkea.")
-    else:
+    elif hemoglobiini < 117:
         print("Hemoglobiinitasosi on alhainen.")
-
