3175b5ae43d5f3169bd66bfd921b915ba5492149
pullavaras <linneagodunov@protonmail.com>
2025-08-31 13:04:46 +0300

    teht2

--- mod-3/teht2.py
+++ mod-3/teht2.py
@@ -1,17 +1,22 @@
+# Tehtävässä on käytettävä if/elif/else-toistorakennetta.
 
-hytti = input("Minkä hyttiluokan valitset; LUX, A, B, vai C? ")
+#    LUX on parvekkeellinen hytti yläkannella.
+#    A on ikkunallinen hytti autokannen yläpuolella.
+#    B on ikkunaton hytti autokannen yläpuolella.
+#    C on ikkunaton hytti autokannen alapuolella.
+
+#Jos käyttäjä syöttää kelvottoman hyttiluokan, ohjelma tulostaa Virheellinen hyttiluokka.
+
+
+hytti = input("Anna hyttiluokka (LUX, A, B tai C): ")
 
 if hytti == "LUX" or hytti == "Lux" or hytti == "lux":
     print("LUX on parvekkeellinen hytti yläkannella.")
-
-elif hytti == "A" or hytti == "a":
+elif hytti == "A" or hytti =="a":
     print("A on ikkunallinen hytti autokannen yläpuolella.")
-
-elif hytti == "B" or hytti == "b":
+elif hytti == "B" or hytti =="b":
     print("B on ikkunaton hytti autokannen yläpuolella.")
-
 elif hytti == "C" or hytti == "c":
     print("C on ikkunaton hytti autokannen alapuolella.")
-
 else:
-    print("Virheellinen hyttiluokka. Valitse yksi listatuista luokista.")
+    print("Virheellinen hyttiluokka.")
