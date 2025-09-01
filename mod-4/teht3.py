eb2084b472184999dbd3146c89345c94bacf9267
pullavaras <linneagodunov@protonmail.com>
2025-09-01 22:47:03 +0300

    ei toimi

--- mod-4/teht3.py
+++ mod-4/teht3.py
@@ -2,3 +2,19 @@
 # kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
 # Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
 
+
+arvo = input("Anna luku: ")
+
+while arvo != "":
+    print(f"Antamasi luku oli: {arvo}")
+    arvo = input("Anna luku: ")
+
+luku = float(arvo)
+if luku < pienin:
+        pienin = luku
+if luku > suurin:
+        suurin = luku
+
+if luku == "":
+    print(f"Pienin annettu luku oli: {pienin}")
+    print(f"Suurin annettu luku oli: {suurin}")
