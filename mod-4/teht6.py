05deb07c92439fa4a0d3e085302e6606deb75e14
pullavaras <linneagodunov@protonmail.com>
2025-09-14 22:12:50 +0300

    korjattu

--- mod-4/teht6.py
+++ mod-4/teht6.py
@@ -1,14 +1,20 @@
 import random
 
-numero = int(input("Kuinka monta osumaa? "))
+syote = input("Kuinka monta pistettä arvotaan? ")
 
-i = 0
-osumat = 0
-while 1 < numero:
-    x = random.random()
-    y = random.random()
-    if x * x + y * y < 1:
-        osumat = osumat + 1
-    i = i + 1
-pi = 4 * osumat / numero
-print(f"Piin arvo: {pi}")
+kokonaispisteet = int(syote)
+ympyrän_sisällä = 0
+pisteet = 0
+
+while pisteet < kokonaispisteet:
+    x = random.randint(-1000, 1000) / 1000
+    y = random.randint(-1000, 1000) / 1000
+
+    if x*x + y*y < 1:
+        ympyrän_sisällä = ympyrän_sisällä + 1
+
+    pisteet = pisteet + 1
+
+pi_arvo = 4 * ympyrän_sisällä / kokonaispisteet
+print(f"Piin likiarvo: {pi_arvo}")
+
