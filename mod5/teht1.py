8fc700ef0e84fe24b88e306ea1fec7460ca0337b
pullavaras <linneagodunov@protonmail.com>
2025-09-17 21:07:48 +0300

    -

--- mod5/teht1.py
+++
@@ -1,4 +0,0 @@
-# Kirjoita ohjelma, joka kysyy käyttäjältä arpakuutioiden lukumäärän.
-# Ohjelma heittää kerran kaikkia arpakuutioita ja tulostaa silmälukujen summan.
-# Käytä for-toistorakennetta.
-
